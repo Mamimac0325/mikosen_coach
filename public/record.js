8b18c97a316a921c4f012c935851c3836d1144d4
Mamio <mail.m.rdet@gmail.com>
2020-05-21 21:07:06 +0900

    APIサーバーのURLを変更しました！

--- public/record.js
+++ public/record.js
@@ -2,51 +2,61 @@ $(function() {
     var areaLog1 = $('#areaLog1')
     var input = $('#record-btn')
 
-    const render = function(arg){
-        var logcontents = $('#logcontents')
-        var row = $('<tr></tr>')
-        row.append(
-            '<td>'+ arg.start + '</td>'+
-            '<td>'+ arg.awake + '</td>'+
-            '<td>'+ arg.feeling + '</td>'+
-            '<td>'+ arg.bath + '</td>'+
-            '<td>'+ arg.howdid + '</td>'
-        )
-    
-        logcontents.append(row)
-    };
-
     input.click(function() {
         var start = $('#start option:selected').text();
         var awake = $('#awake option:selected').text();
         var feeling = $('input[name=feel]:checked').val();
         var bath = $('input[name=bath]:checked').val();
         var howdid = $('#howdid').val();
+    
+        var logcontents = $('#logcontents')
+        var row = $('<tr></tr>')
+        row.append(
+        '<td>'+ start + '</td>'+
+        '<td>'+ awake + '</td>'+
+        '<td>'+ feeling + '</td>'+
+        '<td>'+ bath + '</td>'+
+        '<td>'+ howdid + '</td>'
+        )
 
-        save({
+        logcontents.append(row)
+
+        var savedatas = {
             start:start,
             awake:awake,
             feeling:feeling,
             bath:bath,
             howdid:howdid,
-        });
+        }
 
-        render({
-            start:start,
-            awake:awake,
-            feeling:feeling,
-            bath:bath,
-            howdid:howdid,
-        });
+        save(savedatas);
     });
-
-    try{
-        const datas = load() || []
-        datas.map(function(d){
-            render(d)
-        });
-    } catch {
-
-    }
-
+    load();
 });
+
+
+const save = function (arg){
+    var datas = load()||[];
+    datas.push(arg);
+    // ローカルに保存
+    localStorage.setItem("sleep",JSON.stringify(datas));
+    // サーバーに保存
+    $.ajax({
+        type: "post",
+        url: "/api",
+        data: JSON.stringify(arg),
+        dataType: "json"
+    });
+}
+
+
+// ↓ここにGASからのデータが入るようにする　→コンソールに出るようになる
+const load = function(){
+    //return JSON.parse(localStorage.getItem("sleep"));
+    $.ajax({
+        type: "get",
+        url: "/api",
+        dataType: "json"
+    }).then(r => {console.log(r)});
+
+}
